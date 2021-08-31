e0c712aca4a509e935b5899c554e1c0d677a959e
CrazyBear <root@bear.com>
2021-08-31 23:18:30 +0800

    Initial commit

---
+++ src/main/java/com/fty1/example/account/ExampleAccountApplication.java
@@ -0,0 +1,13 @@
+package com.fty1.example.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleAccountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fty1/example/account/ExampleAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fty1.example.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
